f0e05c4ffa662eedcb8e30ed089db12df0c2e311
jinjoolee1 <jinjoolee126@gmail.com>
2022-04-10 17:32:40 -0500

    Current day HTML fixed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,28 +1,26 @@
 
-// Display today's day and date
+// Today's date
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
 $(document).ready(function () {
-    // saveBtn click listener 
+ 
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
+    
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // Save text in local storage
+    
         localStorage.setItem(time, text);
     })
    
     function timeTracker() {
-        //get current number of hours.
+        //get current time
         var timeNow = moment().hour();
 
-        // loop over time blocks
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-            // To check the time and add the classes for background indicators
             if (blockTime < timeNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
@@ -42,7 +40,7 @@ $(document).ready(function () {
         })
     }
 
-    // Get item from local storage if any
+    // Local storage for time blocks
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
